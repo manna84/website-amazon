55a8be052e3e99e118375d3097dc8cc90c7cd176
manna84 <60120742+manna84@users.noreply.github.com>
2020-07-05 23:27:37 -0400

    added va;idations for signup page

--- server.js
+++ server.js
@@ -54,11 +54,11 @@ app.post("/login",(req, res)=>{
         errors.push("Please enter a Password...!!!")
     }
 
-    // if (req.body.password.length >0 && req.body.password.length < 6) {
-    //     errors.push("Password must contain atleast 6 characters")
-    // }
+    if (req.body.password.length > 0 && req.body.password.length < 6) {
+        errors.push("Password must contain atleast 6 characters")
+    }
 
-    const passValid = (/^(?=.*\d.*)(?=.*[a-z].*)(?=.*[A-Z].*)(?=.*[@#%\*\-+=~\[\]{}<>\?]{6})$/)
+    const passValid = /^(?=.*\d.*)(?=.*[a-z].*)(?=.*[A-Z].*)(?=.*[@#%\*\-+=~\[\]{}<>\?].*)/
 
     if (!req.body.password.match(passValid)) {
         errors.push("Entered password must contain at least a special character, a numeric digit, an uppercase and a lowercase letter")
@@ -66,7 +66,7 @@ app.post("/login",(req, res)=>{
 
     if(errors.length>0) {
         res.render("login", {
-            title : "Login Page",
+            title : "Login Page", 
             errorMessages : errors
         })
     }
@@ -85,6 +85,57 @@ app.get("/signup",(req,res)=>{
 });
 
 app.post("/signup",(req, res)=>{
+
+    const errors = [];
+
+
+    if (req.body.name == "") {
+        errors.push("Please enter a name...!!!")
+    }
+
+    const nameValid = /^(?=.*\d.*)|(?=.*[@#%\*\-+=~\[\]{}<>\?].*)/
+
+    if (req.body.name.match(nameValid)) {
+        errors.push("Name must contain only alphabets")
+    }
+
+    if (req.body.email == "") {
+        errors.push("Please enter an Email...!!!")
+    }
+
+    if (req.body.password == ""){
+        errors.push("Please enter a Password...!!!")
+    }
+
+    if (req.body.confirmPassword == "") {
+        errors.push("Please enter to confirm Password...!!!")
+    }
+
+    if (req.body.password.length > 0 && req.body.password.length < 6) {
+        errors.push("Password must contain atleast 6 characters")
+    }
+
+    const passValid = /^(?=.*\d.*)(?=.*[a-z].*)(?=.*[A-Z].*)(?=.*[@#%\*\-+=~\[\]{}<>\?].*)/
+
+    if (!req.body.password.match(passValid)) {
+        errors.push("Entered password must contain at least a special character, a numeric digit, an uppercase and a lowercase letter")
+    }
+
+    if (req.body.password != req.body.confirmPassword) {
+        errors.push("Passwords are not matching")
+    }
+    
+
+    if(errors.length>0) {
+        res.render("signup", {
+            title: "SignUp Page",
+            errorMessages : errors
+        })
+    }
+
+    else {
+        res.redirect("/");
+    }
     // console.log(`asdasd : ${req.body.password}`)
     // console.log(`asdaasdsd : ${req.body.email}`)
     // console.log(`asdaasdsd : ${req.body.name}`)
