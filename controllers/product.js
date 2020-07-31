a4f5b1dacec0b26394c02b4268db4be83da87f48
manna84 <60120742+manna84@users.noreply.github.com>
2020-07-31 09:44:45 -0400

    validation and data retain done

--- controllers/product.js
+++ controllers/product.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 const product = require("../models/product")
 
-router.get("/",(req, res)=>{
+router.get("/productListing",(req, res)=>{
 
     res.render("productListing", {
         title : "Product Listing Page",
@@ -20,6 +20,97 @@ router.get("/addProduct",(req, res)=>{
 
 });
 
+router.post("/addProduct",(req, res)=>{
 
+    const errors = [];
+
+    if (req.body.name == "") {
+        errors.push("Please enter Product Name...!!!")
+    }
+
+    if (req.body.price == ""){
+        errors.push("Please enter Product Price...!!!")
+    }
+
+    const priceValid = /^(?=.*[a-z].*)|(?=.*[A-Z].*)|(?=.*[@#%\*\-+=~\[\]{}<>\?].*)/
+
+    if (req.body.price.match(priceValid)) {
+        errors.push("Price must contain only digits")
+    }
+
+    if (req.body.description == ""){
+        errors.push("Please enter Product Description...!!!")
+    }
+
+    if (req.body.category == "Select Category"){
+        errors.push("Please select Product Category...!!!")
+    }
+
+    if (req.body.quantity == "Select Quantity"){
+        errors.push("Please select Product quantity...!!!")
+    }
+
+
+    let noRefreshProduct = {
+        name : req.body.name,
+        price : req.body.price,
+        description : req.body.description,
+        category : req.body.category,
+        quantity : req.body.quantity,
+        bestseller : req.body.bestseller
+    }
+
+    if(errors.length>0) {
+        res.render("addProduct", {
+            title : "Add Products", 
+            errorMessages : errors,
+            retainData : noRefreshProduct
+        })
+    }
+
+    else {
+        res.redirect("/")
+    }
+
+    // else {
+
+    //     signupModel.findOne({email:req.body.email})
+    //     .then(user=>{
+
+    //         const errors=[]
+
+    //         if(user==null) {
+
+    //             errors.push("Sorry, your email and/or password is incorrect");
+    //             res.render("login",{
+    //                 errors
+    //             })
+
+    //         } 
+            
+    //         else {
+    //             bcrypt.compare(req.body.password, user.password)
+    //             .then(isMatched=>{
+    //                 if(isMatched) {
+    //                     req.session.userInfo = user;
+    //                     userDashboard(req,res);
+    //                 }
+
+    //                 else {
+    //                     errors.push("Sorry, your email and/or password is incorrect");
+    //                     res.render("login",{
+    //                         errors
+    //                 })
+    //             }
+
+    //             })
+    //             .catch((err)=>console.log(`Error : ${err}`));
+    //                 // res === true
+    //         }
+    //     })
+    //     .catch((err)=>console.log(`Wrong email: ${err}`))
+    // }
+ 
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use((req,res,next)=>{
 })
 
 app.use("/", generalController);
-app.use("/productListing", productController);
+app.use("/", productController);
 
 mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>{
